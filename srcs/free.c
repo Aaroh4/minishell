cc7c9edc8c75834d14c684b0023d02fa130a5a57
Marek Burakowski <mburakow@c1r6p18.hive.fi>
2024-05-31 13:02:06 +0300

    fixed double free

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: mburakow <mburakow@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:06:04 by mburakow          #+#    #+#             */
-/*   Updated: 2024/05/30 15:44:35 by mburakow         ###   ########.fr       */
+/*   Updated: 2024/05/31 12:49:29 by mburakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,24 @@ void	free_args(char **args)
 
 void	free_cmdn(t_cmdn *node)
 {
+	static int r = 0;
+	
 	if (node == NULL)
 		return ;
 	if (node->left)
 		free_cmdn(node->left);
 	if (node->right)
 		free_cmdn(node->right);
+	r++;
 	if (node->cargs)
+	{
 		free_args(node->cargs);
+		dprintf(2, "[%d][%d] Freed cargs\n", r, node->ntype);
+	}
 	free(node->hdocs);
+	dprintf(2, "[%d][%d] Freed hdocs\n", r, node->ntype);
 	free(node->redirs);
+	dprintf(2, "[%d][%d] Freed redirs\n", r, node->ntype);
 	node->cargs = NULL;
 	node->hdocs = NULL;
 	node->redirs = NULL;
@@ -57,13 +65,14 @@ void	free_new_prompt(t_shell *sh)
 		close_ext_pipes(sh);
 	free_args(sh->cmdarr);
 	free_args(sh->cmd);
-	free(sh->hdocs);
+	// free(sh->hdocs);
 	// free(sh->redirs);
 	sh->input = NULL;
 	sh->root = NULL;
 	sh->cmdarr = NULL;
 	sh->cmd = NULL;
 	sh->hdocs = NULL;
+	sh->redirs = NULL;
 }
 
 void	close_ext_pipes(t_shell *sh)
