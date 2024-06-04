191c37f5177b2e92478af8738af192140eb0e5ce
Marek Burakowski <marek.burakowski@protonmail.com>
2024-06-04 15:55:27 +0300

    removed debug info

--- srcs/msenv.c
+++ srcs/msenv.c
@@ -6,7 +6,7 @@
 /*   By: mburakow <mburakow@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:54:02 by mburakow          #+#    #+#             */
-/*   Updated: 2024/06/04 15:39:13 by mburakow         ###   ########.fr       */
+/*   Updated: 2024/06/04 15:54:55 by mburakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ char	**copy_env_vals(int len, char **envp, char **ms_envp, t_shell *sh)
 		ms_envp[i][slen] = '\0';
 		if (ms_envp[i][0] == '_')
 			ms_envp[i][ft_strcpos(ms_envp[i], '.')] = '\0';
-		dprintf(2, "[%d] %s\n", i, ms_envp[i]);
 		i++;
 	}
 	ms_envp[len] = NULL;
